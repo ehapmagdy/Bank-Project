28704e91c879c182385920b75eeb4f1199ac499f
ehapmagdy <ehapmagdytechonlgy6@gmail.com>
2026-06-27 05:07:06 +0300

    Add files via upload

---
+++ Bank Project/Bank Project.cpp
@@ -0,0 +1,21 @@
+﻿#include "Validation.h"
+#include "person.h"
+#include "Client.h"
+#include "Employee.h"
+#include "Admin.h"
+#include "Parser.h"
+#include "FilesHelper.h"
+#include "DataSourceInterface.h"
+#include "FileManager.h"
+#include "AdminService.h"
+#include "EmployeeService.h"
+
+int main() {
+
+  
+
+
+   
+
+    return 0;
+}
